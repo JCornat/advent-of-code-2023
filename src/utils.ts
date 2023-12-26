a7e9398aed356692e84145111daa9c1d641ffa3b
Jacques Cornat <cornat.jacques@gmail.com>
2023-12-26 23:10:36 +0100

    start day 05

--- src/utils.ts
+++ src/utils.ts
@@ -4,7 +4,11 @@ import * as path from 'node:path';
 export async function readLines(filename: string): Promise<string[]> {
   const filePath = path.join(__dirname, '..', 'data', filename);
   const fileContent = await fs.readFile(filePath, 'utf-8');
-  return fileContent.split('\n')
+  return convertStringMultilineToStringArray(fileContent);
+}
+
+export function convertStringMultilineToStringArray(content: string): string[] {
+  return content.split('\n')
     .filter((line) => {
       return line.length > 0;
     });
